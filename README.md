0076ad873e6d6e79cb1a7220c89743cbdd76f613
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-09-10 10:05:49 +0200

    Added wiremock

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # camel-run
+
+
+https://tomd.xyz/camel-testing/
+https://rmannibucau.metawerx.net/camel-blueprint-tests.html
---
+++ camel-rest/src/test/java/com/github/starnowski/camel/fun/WiremockBlueprintTest.java
@@ -0,0 +1,58 @@
+package com.github.starnowski.camel.fun;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.support.DefaultExchange;
+import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class WiremockBlueprintTest extends CamelBlueprintTestSupport {
+
+    @Produce(uri = "direct:sendHttpGetRequest")
+    protected ProducerTemplate inputEndpoint;
+
+    @Rule
+//    @ClassRule
+    public WireMockRule wireMockRule = new WireMockRule(8089);
+
+    @Override
+    protected String getBlueprintDescriptor() {
+        return "OSGI-INF/blueprint/camel-wiremock.xml";
+    }
+
+    protected String setConfigAdminInitialConfiguration(final Properties props) {
+        props.setProperty("service.host", "localhost:" + Integer.toString(wireMockRule.getOptions().portNumber()));
+        // etc...
+
+        return "com.github.starnowski.camel.fun.wiremock.stuff";
+    }
+
+    @Test
+    public void exampleTest() {
+        // GIVEN
+        stubFor(get("/getAllUsers")
+                .withHeader("Content-Type", containing("xml"))
+                .willReturn(ok()
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("<response>SUCCESS</response>")));
+        Exchange senderExchange = new DefaultExchange(context, ExchangePattern.InOut);
+
+        // WHEN
+        Exchange output = inputEndpoint.send(senderExchange);
+
+        // THEN
+        verify(getRequestedFor(urlPathEqualTo("/getAllUsers"))
+                .withHeader("Content-Type", equalTo("text/xml")));
+    }
+
+}
